84ee996dd13b0d2bd74d2ee0c791931da765c700
Ross Wilson <ross.wilson@otago.ac.nz>
2021-08-25 10:49:31 +1200

    complete tutorial

--- R/run_tutorial.R
+++ R/run_tutorial.R
@@ -1,3 +1,11 @@
+#' Cost-Effectiveness Simulation Modelling Tutorial
+#'
+#' Run the 'Cost-Effectiveness Analysis' tutorial on simulation modelling.
+#'
+#' @param name Name of the tutorial to run. Currently only the 'full-model'
+#'     tutorial (using our previously defined model structures to implement a
+#'     modelled cost-effectiveness analysis) is available.
+#'
 #' @export
 run_tutorial <- function(name = "full-model") {
   if (!identical(name, "full-model")) stop("Invalid `name`", call. = TRUE)
